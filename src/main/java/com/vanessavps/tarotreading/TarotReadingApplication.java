6b63553a89c953b50644b5c200e6b1621e3cf976
vanessavps <vanessa.vps29@gmail.com>
2021-05-10 13:15:05 +1200

    It smeels like new code

---
+++ src/main/java/com/vanessavps/tarotreading/TarotReadingApplication.java
@@ -0,0 +1,13 @@
+package com.vanessavps.tarotreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotReadingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TarotReadingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vanessavps/tarotreading/TarotreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessavps.tarotreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarotreadingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
